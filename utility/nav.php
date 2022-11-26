35f29eba8c81b586b0aef3e6b6aefe6f19f22d17
kekecoder <abimbola.felix@gmail.com>
2022-11-26 10:02:40 +0100

    new update

--- utility/nav.php
+++ utility/nav.php
@@ -1,6 +1,3 @@
-<?php
-session_start();
-?>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <a class="navbar-brand" href="#">Navbar</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
